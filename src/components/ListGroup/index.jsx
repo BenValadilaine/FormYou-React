aa0f4720730873ecca422a91cda979e1a49f605b
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-01 20:10:58 +0100

    adding filter logic on room index

--- src/components/ListGroup/index.jsx
+++ src/components/ListGroup/index.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ListGroupItem from '../ListGroupItem/index';
 
 const ListGroup = ({ }) => {
+
+
+    const [activeFilters, setActiveFilters] = useState([]);
+
+    const handleFilter = (value) => {
+        if (activeFilters.find((e) => e.id === value.id)) {
+            let updatedFilter = activeFilters.find((e) => e.id === value.id);
+            let otherFilters = activeFilters.filter((e) => e.id != value.id);
+            updatedFilter = { ...updatedFilter, ...value };
+            setActiveFilters([...otherFilters, updatedFilter])
+        } else {
+            setActiveFilters([...activeFilters, value]);
+        }
+
+    }
+
+    useEffect(()=>{
+
+        console.log(activeFilters)
+
+    }, [activeFilters])
+
     return (
         <ul className="list-group d-block overflow-auto" style={{ height: "45vh" }}>
 
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
-            <ListGroupItem />
+
+
+            {
+                Array.from(Array(10).keys()).map((e) => {
+
+                    return <ListGroupItem key={e} name={`category_${e}`} id={e} text={`Categorie ${e}`} handleFilter={(value) => { handleFilter(value) }} />
+
+                })
+            }
+
+
 
         </ul>
     )
--- src/components/ListGroupItem/index.jsx
+++ src/components/ListGroupItem/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 
-const ListGroupItem = ({}) =>
-{
+const ListGroupItem = ({ handleFilter, name, id, text }) => {
+    const handleClick = (event) => {
+        handleFilter({ name: event.target.name, id: event.target.id, checked: event.target.checked });
+    }
     return (
-        <li className="list-group-item"><input type="checkbox" name="" id="" className="mr-4" /> Nom de la catégorie</li>
+        <li className="list-group-item"><input type="checkbox" name={name} id={id} className="mr-4" onClick={handleClick} />{text}</li>
     )
 }
 
