0387680d48e925eb86095e34aafbd9b9e4380154
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-01 14:19:19 +0100

    adding signup page layout

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,12 +1,66 @@
 import React from "react";
 import LoginForm from "../components/Forms/SigninForm";
 // import { FormattedMessage } from 'react-intl';
+import signinImage from "../assets/img/signin.jpg";
 
 const Login = () => {
 	return (
-		<div className="login-page">
-			<h1>Log in</h1>
-			<LoginForm />
+		<div className="container d-flex flex-column justify-content-center" style={{ minHeight: "95vh" }}>
+
+				<div className="row col-12 border-0 p-0 ">
+
+					<div className="col-6 p-0 d-none d-lg-block border-neumorphic border-rounded" style={{ height: "75vh" }}> 
+					
+
+							<img src={signinImage} alt="signin image" className="h-100 w-100 border-rounded m-0 p-0"/>
+
+
+
+					</div>
+
+					<div className="col-lg-5 offset-lg-1 col-12 border-neumorphic bg-white p-4 d-flex flex-column justify-content-around border-rounded" style={{ height: "75vh" }}>
+
+						<form action="">
+
+
+							<h1>CONNEXION</h1>
+
+
+							<div className="form-group">
+
+								<label htmlFor="">Adresse email:</label>
+
+								<input type="text" className="form-control"/>
+
+
+							</div>
+
+
+							<div className="form-group">
+
+								<label htmlFor="">Mot de passe:</label>
+
+								<input type="text" className="form-control"/>
+
+
+							</div>
+
+
+
+							<button className="btn btn-scheme-2 btn-lg col-12" type="submit">VALIDER</button>
+
+						
+
+
+
+						</form>
+
+					</div>
+
+
+				</div>
+		
+
 		</div>
 	);
 };
