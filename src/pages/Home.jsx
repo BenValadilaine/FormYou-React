29a3638b2f36d8da9fa716c82a19aa2023fee685
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-01 10:59:38 +0100

    adding layout home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1>Home sweet home</h1>
+
+    <div className="container-fluid m-auto" style={{ minHeight: "80vh" }}>
+
+
+      <div className="row">
+
+        <div className="col-4">
+
+
+
+
+        </div>
+
+        <div className="col-8">
+
+
+
+          <h1>Formez vous et vos collaborateurs tout au long de de votre vie grâce à FormYou &copy;</h1>
+
+
+          <h2 className="dark-light">Une solution innovante pour mettre l’entreprise et ses salariés au coeur du parcours individuel de formation</h2>
+
+          <h3>Des aides sont possibles, demander conseil à nos experts ...</h3>
+
+
+          <button className="btn btn-lg col-md-4 btn-white">Les formations</button>
+
+        </div>
+
+
+      </div>
+
+
+    </div>
   )
 }
 
