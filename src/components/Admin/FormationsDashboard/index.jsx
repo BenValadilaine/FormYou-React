ebbb910dc4fd9bf1585553fe42666f8c447e9935
JulietDeRozario <juliet.dr@tutanota.com>
2020-12-02 22:12:21 +0100

    index formation in admin dashboard added

--- src/components/Admin/FormationsDashboard/index.jsx
+++ src/components/Admin/FormationsDashboard/index.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import AddIcon from "../../../assets/icons/plus.svg";
 import EditIcon from "../../../assets/icons/edit.svg";
+import API_REQUEST from '../../../services/ApiRequest/ApiRequest';
+import Cookies from "js-cookie";
+import { Modal, Button } from "react-bootstrap";
+
 
 const LessonsDashboard = () => {
+
+	//STATES
+	const [show, setShow] = useState(false);
+	const [formations, setFormations] = useState([]);
+	const [formationSelected, setFormationSelected] = useState({});
+
+	const handleClose = () => setShow(false);
+	const handleShow = () =>  setShow(true);
+
+	useEffect(() => {
+		handleFormations();
+	}, [show])
+
+	const handleFormations = async () => {
+	
+		// request to /users
+		const response = await API_REQUEST.find("/formations");
+		setFormations(response);
+	}
+
 	return (
 		<div>
 			<table className="table">
@@ -12,7 +36,7 @@ const LessonsDashboard = () => {
 						<th scope="col">Nom</th>
 						<th scope="col">Description</th>
 						<th scope="col">Professeur</th>
-						<th scope="col">Catégorie</th>
+						<th scope="col">Catégories</th>
 						<th scope="col">Créée le</th>
 						<th scope="col">
 							<a href="#">
@@ -22,28 +46,44 @@ const LessonsDashboard = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>React.Js</td>
-						<td>
-							React (aussi appelé React.js ou ReactJS) est une bibliothèque
-							JavaScript libre développée par Facebook depuis 2013. Le but
-							principal de cette bibliothèque est de faciliter la création
-							d'application web monopage, via la création de composants
-							dépendant d'un état et générant une page (ou portion) HTML à
-							chaque changement d'état.
-						</td>
-						<td>L'ami Paul</td>
-						<td>Developpement web</td>
-						<td>18/12/2020</td>
-						<td>
-							<a href="#">
-								<img src={EditIcon} alt="Éditer" width="25" height="25" />
-							</a>
-						</td>
-					</tr>
+					{ formations.map((formation) => (
+						<tr>
+							<th scope="row">{formation.id}</th>
+							<td>{formation.name}</td>
+							<td>{formation.description}</td>
+							<td>{formation.teacher_id}</td>
+							<td>Developpement web</td>
+							<td>{formation.created_at}</td>
+							<td>
+								<a href="#">
+									<img
+										src={EditIcon}
+										alt="Éditer"
+										width="25"
+										height="25"
+										onClick={handleShow}
+									/>
+								</a>
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+				<Modal.Title>{`${formationSelected.first_name} ${formationSelected.last_name}`}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Mettre à jour le statut de cet utilisateur</Modal.Body>
+				<Modal.Footer>
+				<Button variant="success" onClick={handleClose}>
+					Valider
+				</Button>
+				<Button variant="danger" onClick={handleClose}>
+					Refuser
+				</Button>
+					</Modal.Footer>
+			</Modal>
 		</div>
 	);
 };
