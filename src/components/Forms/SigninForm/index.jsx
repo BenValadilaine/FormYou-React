1ef590895d3d404c326acdfc44e66db2428effe0
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-12-02 18:09:19 +0100

    big bug with You need to sign in or sign up before continuing

--- src/components/Forms/SigninForm/index.jsx
+++ src/components/Forms/SigninForm/index.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import { FormattedMessage } from 'react-intl';
 import signinImage from "../../../assets/img/signin.jpg";
 import FormGroup from '../../FormGroup/index';
+import API_REQUEST from '../../../services/ApiRequest/ApiRequest';
+import { API_ENDPOINTS } from "../../../services/ApiRequest/config/config.js";
+import { useDispatch } from 'react-redux';
+import { setCurrentUser } from "../../../redux/actions";
+import Cookies from "js-cookie";
+import { useHistory } from "react-router-dom";
 
 const SigninForm = () => {
+
+  //STATES
+  const [userDatas, setUserDatas] = useState({});
+
+  // importing dipatch actions
+	const dispatch = useDispatch();
+
+	// importing history form react router dom
+	const history = useHistory();
+
+  //analysing submit form
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    setUserDatas({...userDatas, email: email, password: password});
+    handleRegistration();
+  }
+
+  const handleRegistration = async () => {
+
+    // destructuring object userDatas state containing user registration infos
+    const { email, password } = userDatas
+
+    // request to /signup token
+    const response = await API_REQUEST.signIn(
+      {
+        user: {
+          email: email,
+          password: password
+        }
+      }
+      , API_ENDPOINTS["signin"]);
+
+    // accessing jwt token
+    const jwt = response.headers.get("Authorization");
+    Cookies.set("jwt_token", jwt)
+
+    // accessing data of response
+    const current_user = await response.json().data;
+
+    // constructing payload
+    const payload = {
+      current_user
+    }
+
+    // dispatching action to redux store
+    dispatch({
+      type: "SET_CURRENT_USER",
+      payload
+    });
+
+    history.push("/")
+  }
+
   return (
     <div className="container d-flex flex-column justify-content-center" style={{ minHeight: "95vh" }}>
 
@@ -30,7 +93,7 @@ const SigninForm = () => {
             <FormGroup label="Mot de passe:" inputName="password" inputId="password" inputType="password" />
 
 
-            <button className="btn btn-scheme-2 btn-lg col-12 my-4" type="submit">VALIDER</button>
+            <button className="btn btn-scheme-2 btn-lg col-12 my-4" type="submit" onClick={(event) => handleClick(event)}>VALIDER</button>
 
 
 
