7c034efb1d73cf86cff490240c5d744bb1a913a5
Ben <benjamin.valadilaine@gmail.com>
2020-12-02 17:19:36 +0100

    before merge

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 import "./index.scss";
 
 const Navbar = () => {
