046ae79f26ba280e398341eab30d8f820d3f3452
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-02 19:36:07 +0100

    adding formationPage component fetch data api looking for formation session to display in calendar and detail of faramation to be displayed in CardFormationDetail

--- src/components/Formation/index.jsx
+++ src/components/Formation/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { capitalize } from "../../helpers/string";
 
 
 const Formation = ({ id, title, description, created_at, updated_at }) => {
 
+    const history = useHistory()
+    const redirect = (id) => {
+
+        history.push(`/formation/${id}`)
+
+
+    }
     return (
-        <div className="card p-4 border-rounded" id={id}>
+        <div className="card p-4 border-rounded" onClick={() => redirect(id)} id={id}>
 
 
             <div className="row">
