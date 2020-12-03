42fa39f6d00cd5d03961e015cb9be6d4a4a6483e
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-03 16:45:15 +0100

    adding udpate method to profile

--- src/components/Forms/EditProfileForm/index.jsx
+++ src/components/Forms/EditProfileForm/index.jsx
@@ -1,16 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import FormGroup from '../../FormGroup/index';
 import jwt_decode from 'jwt-decode';
 import Cookies from 'js-cookie';
 import API_REQUEST from "../../../services/ApiRequest/ApiRequest";
-import {API_ENDPOINTS} from "../../../services/ApiRequest/config/config";
+import { API_ENDPOINTS } from "../../../services/ApiRequest/config/config";
 
 const EditProfileForm = ({ }) => {
 
     const [userDatas, setUserDatas] = useState({});
 
 
-    useEffect(()=>{
+    const handleUpdateProfile = async (event) => {
+
+        event.preventDefault();
+
+        const firstNameInputValue = document.querySelector("#firstname").value;
+        const lastNameInputValue = document.querySelector("#lastname").value;
+        const emailInputValue = document.querySelector("#lastname").value;
+        const passwordInputValue = document.querySelector("#password").value;
+        const passwordConfirmationValue = document.querySelector("#password_confirmation").value;
+        const ageInputValue = document.querySelector("#age").value;
+
+        if (firstNameInputValue != "" && lastNameInputValue != "" && emailInputValue != "" && passwordInputValue != "" && passwordConfirmationValue != "" && ageInputValue != "") {
+            if (passwordInputValue == passwordConfirmationValue) {
+                const jwt_token = Cookies.get('jwt_token');
+                const user_id = jwt_decode(jwt_token).sub;
+
+
+                const user_role = await API_REQUEST.find(API_ENDPOINTS["users"] + `/${user_id}/roles`, true, jwt_token);
+
+                const response = await API_REQUEST.update(
+
+                    {
+                        "user": {
+                            "first_name": firstNameInputValue,
+                            "last_name": lastNameInputValue,
+                            "email": emailInputValue,
+                            "password": passwordInputValue,
+                            "password_confirmation": passwordConfirmationValue,
+                            "age": ageInputValue,
+                            "role_id": user_role.id
+                        }
+                    },
+
+                    API_ENDPOINTS["users"] + `/${user_id}`, true, jwt_token
+                ).then((e)=>e.json())
+
+
+                console.log(response)
+
+
+            }
+        } else {
+            console.error("All fields must be filled");
+        }
+    }
+
+
+    useEffect(() => {
 
         const fetchUserDatas = async () => {
             const jwt_token = Cookies.get('jwt_token');
@@ -18,9 +65,9 @@ const EditProfileForm = ({ }) => {
             const response = await API_REQUEST.find(API_ENDPOINTS["users"] + `/${user_id}`, true, jwt_token)
             setUserDatas(response)
         }
-    
+
         fetchUserDatas()
-    
+
 
     }, [])
 
@@ -34,12 +81,14 @@ const EditProfileForm = ({ }) => {
 
             <FormGroup label="Nom:" inputName="lastname" inputId="lastname" inputType="text" />
 
-            <FormGroup label="Adresse email:" inputName="email" inputId="email" inputType="text" value={userDatas?.data?.attributes?.email}/>
+            <FormGroup label="Age:" inputName="age" inputId="age" inputType="number" />
+
+            <FormGroup label="Adresse email:" inputName="email" inputId="email" inputType="text" value={userDatas?.data?.attributes?.email} />
             <FormGroup label="Mot de passe:" inputName="password" inputId="password" inputType="password" />
             <FormGroup label="Confirmer le mot de passe:" inputName="password_confirmation" inputId="password_confirmation" inputType="password" />
 
 
-            <button className="btn btn-scheme-2 btn-lg col-12 my-4" type="submit">VALIDER</button>
+            <button className="btn btn-scheme-2 btn-lg col-12 my-4" type="submit" onClick={handleUpdateProfile}>VALIDER</button>
 
 
         </form>
