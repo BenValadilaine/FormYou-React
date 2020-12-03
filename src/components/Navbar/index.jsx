fa3e03bd35e34942a6ecb5e763513d85c3b3ae4b
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-12-03 11:54:50 +0100

    try to useEffect on redux store

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 import { useSelector } from "react-redux";
 import API_REQUEST from "../../services/ApiRequest/ApiRequest";
 import { API_ENDPOINTS } from "../../services/ApiRequest/config/config";
@@ -7,6 +8,8 @@ import "./index.scss";
 import Cookies from "js-cookie";
 
 const Navbar = () => {
+	const [isConnected, setIsConnected] = useState(false);
+	const dispatch = useDispatch();
 	const currentUser = useSelector((state) => state.current_user);
 
 	const handleSignOut = async () => {
@@ -20,110 +23,99 @@ const Navbar = () => {
 		}
 	};
 
-	const display = () => {
+	useEffect(() => {
+
 		if (currentUser) {
-			return (
-				<nav className="navbar navbar-expand-lg navbar-light">
-					<Link to="/" className="navbar-brand" id="nav-brand" title="home">
-						FormYou
-					</Link>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav ml-auto mr-auto">
-							<li className="nav-item">
-								<Link to="/formations" className="nav-link">
-									Les formations
-								</Link>
-							</li>
-
-							<li className="nav-item">
-								<Link to="/contact" className="nav-link">
-									Nous contacter
-								</Link>
-							</li>
-						</ul>
-
-						<ul className="navbar-nav flex mr-5" id="nav-connect">
-							<li className="nav-item">
-								<Link to="/profile" className="nav-link">
-									Mon Profil
-								</Link>
-							</li>
-
-							<li className="nav-item">
-								<a
-									href="#"
-									onClick={() => handleSignOut()}
-									className="nav-link"
-								>
-									Se déconnecter
-								</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			);
+			setIsConnected(true)
 		} else {
-			<nav className="navbar navbar-expand-lg navbar-light">
-				<Link to="/" className="navbar-brand" id="nav-brand" title="home">
-					FormYou
-				</Link>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav ml-auto mr-auto">
-						<li className="nav-item">
-							<Link to="/formations" className="nav-link">
-								Les formations
-							</Link>
-						</li>
-
-						<li className="nav-item">
-							<Link to="/signup" className="nav-link">
-								Nous rejoindre
-							</Link>
-						</li>
-
-						<li></li>
-
-						<li className="nav-item">
-							<Link to="/contact" className="nav-link">
-								Nous contacter
-							</Link>
-						</li>
-					</ul>
-
-					<ul className="navbar-nav flex mr-5" id="nav-connect">
-						<li className="nav-item">
-							<Link to="/signin" className="nav-link">
-								Se connecter
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>;
+			if (Cookies.get("jwt_token")) {
+				const payload = { Cookies.get("jwt_token") }
+				dispatch({
+					type: "SET_CURRENT_USER",
+					payload
+				});
+			} else {
+				setIsConnected(false)
+			}
 		}
-	};
-	return display();
+		/*
+		const isLogged = async () => {
+			await dispatch
+		}
+
+		if (Cookies.get("jwt_token")) {
+			setIsConnected(true)
+		} else {
+			setIsConnected(false)
+		}*/
+		console.log(Cookies.get("jwt_token"));
+		console.log(isConnected);
+		console.log(currentUser);
+	}, [dispatch])
+
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light">
+			<Link to="/" className="navbar-brand" id="nav-brand" title="home">
+				FormYou
+			</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav ml-auto mr-auto">
+					<li className="nav-item">
+						<Link to="/formations" className="nav-link">
+							Les formations
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/contact" className="nav-link">
+							Nous contacter
+						</Link>
+					</li>
+				</ul>
+
+				<ul className="navbar-nav flex mr-5" id="nav-connect">
+					<li className="nav-item">
+						<Link to="/signup" className="nav-link">
+							Nous rejoindre
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/signin" className="nav-link">
+							Se connecter
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<Link to="/profile" className="nav-link">
+							Mon Profil
+						</Link>
+					</li>
+
+					<li className="nav-item">
+						<a
+							href="#"
+							onClick={() => handleSignOut()}
+							className="nav-link"
+						>
+							Se déconnecter
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
